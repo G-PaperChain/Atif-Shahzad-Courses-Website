ef190da48829b6bcd7cc47be60f9bb33034499c3
G-PaperChain <g.paperchain@gmail.com>
2025-09-14 20:18:16 +0300

    run.py

--- backend/run.py
+++ backend/run.py
@@ -2,9 +2,7 @@ from app import create_app
 import os
 import sys
 
-# Add current directory to Python path
-# it's me I guess
-# sys.path.insert(0, os.path.dirname(__file__))
+sys.path.insert(0, os.path.dirname(__file__))
 
 app = create_app()
 
