b3799b6146b3d83a51b08779932cd88185752ac0
G-PaperChain <g.paperchain@gmail.com>
2025-09-15 16:34:30 +0300

    Url prblem fix

--- frontend/src/Components/AuthComponents/AuthContext.jsx
+++ frontend/src/Components/AuthComponents/AuthContext.jsx
@@ -19,7 +19,7 @@ export const useAuth = () => {
 };
 
 export const AuthProvider = ({ children }) => {
-  const API_BASE = "https://dratifshahzad.com/api"
+  const API_BASE = "https://dratifshahzad.com"
 
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
